d504384907471cb353acf66862b6e3f74d74f9b1
alex <alexander.hernandez@usda.gov>
2024-01-25 15:00:25 -0700

    additional changes

--- testing.R
+++ testing.R
@@ -3,3 +3,12 @@
 library(dplyr)
 library(sf)
 library(terra)
+library(raster)
+library(mapview)
+
+###
+setwd("/media/geodata/shared/Turf/Greenville")
+library(here)
+
+## load geopackage with all the VIs
+Green.plots<-read_sf(here("ToZonal/Greenville_plots_2024zonal11.gpkg"))
