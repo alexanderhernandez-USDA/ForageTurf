707cf9893bc6421b2a56c24996ff234942fb6bf5
alex <alexander.hernandez@usda.gov>
2024-01-25 15:18:40 -0700

    additional changes2

--- testing.R
+++ testing.R
@@ -5,10 +5,13 @@ library(sf)
 library(terra)
 library(raster)
 library(mapview)
-
+library(excelR)
 ###
 setwd("/media/geodata/shared/Turf/Greenville")
 library(here)
 
 ## load geopackage with all the VIs
 Green.plots<-read_sf(here("ToZonal/Greenville_plots_2024zonal11.gpkg"))
+
+# Load Shaun's field data for Y2023
+VisualRat2023<-readxl::read_excel(here("Greenvilleratings22_23.xlsx"), sheet = "KBG_2023")
